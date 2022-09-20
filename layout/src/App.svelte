8315a1a38e177794f8fb8e72e54e9fd7148de575
Jakob Ankarhem <jakob.ankarhem@jetshop.se>
2022-09-20 12:40:32 +0200

    fix layout

--- layout/src/App.svelte
+++ layout/src/App.svelte
@@ -2,15 +2,22 @@
   import './app.css';
 </script>
 
-<nav class="test">
-  <ol class="bg-gray-400 w-48 h-screen text-white">
+<header class="fixed inset-x-0 top-0 flex items-center justify-end h-14 px-4">
+  <p>hello</p>
+</header>
+
+<nav class="fixed inset-y-0 min-w-[320px] h-screen">
+  <ol>
     <li class="text-white text text-4xl">checkout</li>
     <li>Product</li>
   </ol>
 </nav>
 
 <style>
-  .test {
-    background: green;
+  /* Move main when layout is used */
+  :global(main) {
+    margin-left: 320px;
+    margin-top: 3.5rem;
+    min-height: calc(100vh - 3.5rem);
   }
 </style>
